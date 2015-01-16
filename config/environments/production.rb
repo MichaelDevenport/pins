7043ef0158d67fc00ebdd614f10a39adb1ba0367
Michael <russellyates2013@gmail.com>
2015-01-16 22:36:22 +1300

    fix deploy bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Pinteresting::Application.configure do
+Pins::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
