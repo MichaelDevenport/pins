c0237197d0a998432316bdc53ce85b8cce34cfff
Michael <russellyates2013@gmail.com>
2015-02-07 06:11:38 +1300

    Added landing page and styled pages a bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :disable_nav, only: [:landing]
+
+  def disable_nav
+  	@disable_nav = true
+	end
 
   protected
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+
   def home
   end
 
   def about
+  end
+
+  def landing
+  	@disable_navbar = true
   end 
 end
