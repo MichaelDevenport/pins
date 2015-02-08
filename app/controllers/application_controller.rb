6b3f66fa465da09e4449b6b1fb9c38cd9d740296
Michael <russellyates2013@gmail.com>
2015-02-09 03:18:56 +1300

    reinstall Raty plugin with associated css, precompiled assets locally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  before_filter :disable_nav, only: [:landing]
-
-  def disable_nav
-  	@disable_nav = true
-	end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
+  def disable_nav
+    @disable_nav = true
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+	before_filter :disable_nav, only: [:landing]
 
   def home
   end
