974b8a92b43763c8dcef4d9db08ec7f3a8fc2416
Michael <russellyates2013@gmail.com>
2015-05-10 23:55:08 +1200

    reordering  require bootstrap and precompiled locally

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
 //= require jquery.turbolinks
 //= require turbolinks
 //= require masonry/jquery.masonry
