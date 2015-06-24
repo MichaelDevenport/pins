30ed4b61bfcbee7bf14044d52f97a3ecc47fe9f2
Michael <russellyates2013@gmail.com>
2015-06-25 02:38:19 +1200

    Force users to sign in to view show page with a before action exception only on index for pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,7 +2,7 @@ class PinsController < ApplicationController
   before_action :find_pin, only: [:show, :edit, :update, :destroy]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
 
   def search
     if params[:search].present?
